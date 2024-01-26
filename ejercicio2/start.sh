cd1acc50cb9b3e66cb9c71e26c5c3350eb8b7f0b
esnieto <esnieto@bancolombia.com.co>
2024-01-26 14:31:08 -0500

    actualizacion de argumentos y notas para ejecución

--- ejercicio2/start.sh
+++ ejercicio2/start.sh
@@ -4,9 +4,9 @@ LGREEN='\033[1;32m'
 NC='\033[0m'
 LBLUE='\033[0;34m'
 LYELLOW='\033[1;33m'
-repo="bootcamp-devops-2023"
+repo="app-295devops-travel"
 project="app-295devops-travel"
-branch="clase2-linux-bash"
+branch="main"
 
 if [ -d "$repo" ] ; then
     echo "Existe repo"
@@ -15,9 +15,11 @@ if [ -d "$repo" ] ; then
     git checkout $branch
     #llamado a la función de copia de archivos estaticos
     echo -e "\n${LBLUE} Archivos estaticos copiados.${NC}"
+    docker-compose up -d --build
 else
     echo -e "\n${LYELLOW} Se clonará el repositorio.${NC}"
     git clone https://github.com/roxsross/$repo.git
     cd $repo/
     git checkout $branch
+    docker-compose up -d --build
 fi
